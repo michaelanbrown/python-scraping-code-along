c240d13526a23f5e30fd7a6ebfa6d483c81d84ae
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-22 10:14:10 -0500

    defined the __str__ method

--- lib/Course.py
+++ lib/Course.py
@@ -3,3 +3,9 @@ class Course:
     self.title = title
     self.schedule = schedule
     self.description = description
+
+    def __str__(self):
+    output = ''
+    output += f'Title: {self.title}\nSchedule: {self.schedule}\nDescription: {self.description}\n'
+    output += '------------------'
+    return output
