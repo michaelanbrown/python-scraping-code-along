a2731700cac470b78090bd46a9ff7efeaa0c1616
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-22 10:13:50 -0500

    resolved typo

--- lib/Course.py
+++ lib/Course.py
@@ -2,4 +2,4 @@ class Course:
     def __init__(self, title, schedule, description):
     self.title = title
     self.schedule = schedule
-    self.description = descriptio
+    self.description = description
