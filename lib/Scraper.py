eff2a400b7c2195db608f1a6a9b515ff29f2e259
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-23 10:37:25 -0500

    defined the get page method

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -8,3 +8,6 @@ class Scraper:
     def __init__(self):
       self.courses = []
 
+    def get_page():
+        doc =  BeautifulSoup(requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
+        ipdb.set_trace()
