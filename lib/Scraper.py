00857935ea8dabebd3d2786f972de834bb0e64a2
michaelanbrown <m_brownbrown@yahoo.com>
2025-01-23 10:38:31 -0500

    added more to the get page method in scraper

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -8,8 +8,17 @@ class Scraper:
     def __init__(self):
       self.courses = []
 
-    def get_page():
+    def get_page(self):
         doc =  BeautifulSoup(requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
-        ipdb.set_trace()
+
+        for course in doc.select('.post'):
+            print(type(course))
+
+            title = course.select("h2")[0].text if course.select("h2") else ''
+            date = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text  if course.select("p") else ''
+
+            new_course = Course(title, date, description)
+            self.courses.append(new_course)
 
 Scraper.get_page()
